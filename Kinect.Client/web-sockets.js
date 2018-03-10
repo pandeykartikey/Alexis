fca92eaeea987f314417423d22f0cdd9953b6bb6
Kartikey Pandey <pandeykartikey99@gmail.com>
2018-03-10 08:35:57 -0800

    Initial commit

---
+++ Kinect.Client/web-sockets.js
@@ -0,0 +1,81 @@
+﻿window.onload = function () {
+    var status = document.getElementById("status");
+    var canvas = document.getElementById("canvas");
+    var buttonColor = document.getElementById("color");
+    var buttonDepth = document.getElementById("depth");
+    var context = canvas.getContext("2d");
+
+    var camera = new Image();
+
+    camera.onload = function () {
+        context.drawImage(camera, 0, 0);
+    }
+
+    if (!window.WebSocket) {
+        status.innerHTML = "Your browser does not support web sockets!";
+        return;
+    }
+
+    status.innerHTML = "Connecting to server...";
+
+    // Initialize a new web socket.
+    var socket = new WebSocket("ws://localhost:8181");
+
+    // Connection established.
+    socket.onopen = function () {
+        status.innerHTML = "Connection successful.";
+    };
+
+    // Connection closed.
+    socket.onclose = function () {
+        status.innerHTML = "Connection closed.";
+    }
+
+    // Receive data FROM the server!
+    socket.onmessage = function (event) {
+        if (typeof event.data === "string") {
+            // SKELETON DATA
+
+            // Get the data in JSON format.
+            var jsonObject = JSON.parse(event.data);
+
+            // Display the skeleton joints.
+            for (var i = 0; i < jsonObject.skeletons.length; i++) {
+                for (var j = 0; j < jsonObject.skeletons[i].joints.length; j++) {
+                    var joint = jsonObject.skeletons[i].joints[j];
+
+                    // Draw!!!
+                    context.fillStyle = "#FF0000";
+                    context.beginPath();
+                    context.arc(joint.x, joint.y, 10, 0, Math.PI * 2, true);
+                    context.closePath();
+                    context.fill();
+                }
+            }
+        }
+        else if (event.data instanceof Blob) {
+            // RGB FRAME DATA
+            // 1. Get the raw data.
+            var blob = event.data;
+
+            // 2. Create a new URL for the blob object.
+            window.URL = window.URL || window.webkitURL;
+
+            var source = window.URL.createObjectURL(blob);
+
+            // 3. Update the image source.
+            camera.src = source;
+
+            // 4. Release the allocated memory.
+            window.URL.revokeObjectURL(source);
+        }
+    };
+
+    buttonColor.onclick = function () {
+        socket.send("Color");
+    }
+
+    buttonDepth.onclick = function () {
+        socket.send("Depth");
+    }
+};
