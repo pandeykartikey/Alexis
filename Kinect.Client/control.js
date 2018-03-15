05b3e96c0e55b19f3864d9dd8f48a19a8faaff9f
Vedansh Bhartia <vedanshbhartia@gmail.com>
2018-03-15 18:28:48 +0530

    Partial work done for eraser and brush controls; bug fixes

--- Kinect.Client/control.js
+++ Kinect.Client/control.js
@@ -3,6 +3,9 @@ const radius = 0.5;
 var hand1 = document.getElementById('left');
 var hand2 = document.getElementById('right');
 
+var brush = document.getElementById('brush');
+var eraser = document.getElementById('eraser');
+
 var color1 = document.getElementById('color1');
 var color2 = document.getElementById('color2');
 var color3 = document.getElementById('color3');
@@ -14,6 +17,7 @@ var color7 = document.getElementById('color7');
 function changeColor(color, hand){
 	console.log(color);
 	hand.setAttribute('color', color);
+	MESH_DEFAULT_COLOR = color;
 }	
 
 
@@ -21,17 +25,17 @@ function checkCollision(color, hand, call_func){
 
 	var pos1 = color.getAttribute('position'),
 	pos2 = hand.getAttribute('position');
-	console.log(JSON.stringify(pos1)+JSON.stringify(pos2));
 	var x1 = pos1.x,
 	x2 = pos2.x,
-	y1 = pos2.y,
-	y2 = pos1.y,
-	z1 = pos2.z,
-	z2 = pos1.z;
+	y1 = pos1.y,
+	y2 = pos2.y,
+	z1 = pos1.z,
+	z2 = pos2.z;
 
 	if((Math.sqrt(Math.pow((x1-x2),2) + Math.pow((y1-y2),2) + Math.pow((z1-z2),2))) < radius){
 		var setColor = color.getAttribute('color');
-		console.log((Math.sqrt(Math.pow((x1-x2),2) + Math.pow((y1-y2),2) + Math.pow((z1-z2),2))));
+	// console.log(JSON.stringify(pos1)+JSON.stringify(pos2));
+		// console.log((Math.sqrt(Math.pow((x1-x2),2) + Math.pow((y1-y2),2) + Math.pow((z1-z2),2))));
 		call_func(setColor, hand);
 	}
 
--- Kinect.Client/web-sockets.js
+++ Kinect.Client/web-sockets.js
@@ -9,8 +9,52 @@ var OFFSET_Z = -15;
 var MESH_LINE_WIDTH = '10';
 var MESH_DEFAULT_COLOR = '#E20049';
 
+function draw(initial, final, scene, el){
+
+    var mesh_coords = initial.x + ' ' + initial.y + ' ' + initial.z + ', ' + final.x + ' ' + final.y + ' ' + final.z;
+
+    var mesh = document.createElement('a-entity');
+    var mesh_properties = {
+        lineWidth: MESH_LINE_WIDTH,
+        path: mesh_coords,
+        color: MESH_DEFAULT_COLOR
+    };
+
+    mesh.setAttribute('meshline', mesh_properties);
+    mesh.setAttribute('class', 'mesh');
+    scene.appendChild(mesh);
+
+    el.setAttribute('position', final.x + " " + final.y + " " + final.z);
+}
+
+function erase(initial, final, scene, el){
+    document.getElementsByClassName('mesh');
+    for (line in mesh) {
+        var line_coords = {
+            x: line_coords.getAttribute('position').x,
+            y: line_coords.getAttribute('position').y,
+            z: line_coords.getAttribute('position').z
+        },
+
+        x1 = final.x,
+        x2 = line_coords.x,
+        y1 = final.y,
+        y2 = line_coords.y,
+        z1 = final.z,
+        z2 = line_coords.z,
+        dist = (Math.sqrt(Math.pow((x1-x2),2) + Math.pow((y1-y2),2) + Math.pow((z1-z2),2)));
+        
+        if(dist < radius){
+            line.remove();
+        }
+
+        el.setAttribute('position', final.x + " " + final.y + " " + final.z);
+    }
+}
+
 window.onload = function () {
     var scene = document.querySelector("#scene");
+    var actionFunction = draw;
 
     status.innerHTML = "Connecting to server...";
 
@@ -43,28 +87,16 @@ window.onload = function () {
                             y: el.getAttribute('position').y,
                             z: el.getAttribute('position').z
                         },
-                            final = {
-                                x: (joint.x * SCALE_X + OFFSET_X),
-                                y: (joint.y * SCALE_Y + OFFSET_Y),
-                                z: (joint.z * SCALE_Z + OFFSET_Z)
-                            }
+                        final = {
+                            x: (joint.x * SCALE_X + OFFSET_X),
+                            y: (joint.y * SCALE_Y + OFFSET_Y),
+                            z: (joint.z * SCALE_Z + OFFSET_Z)
+                        }
 
-                        var mesh_coords = initial.x + ' ' + initial.y + ' ' + initial.z + ', ' + final.x + ' ' + final.y + ' ' + final.z;
-
-                        var mesh = document.createElement('a-entity');
-                        var mesh_properties = {
-                            lineWidth: MESH_LINE_WIDTH,
-                            path: mesh_coords,
-                            color: MESH_DEFAULT_COLOR
-                        };
-
-                        mesh.setAttribute('meshline', mesh_properties);
-                        scene.appendChild(mesh);
-
-                        el.setAttribute('position', final.x + " " + final.y + " " + final.z);
+                        actionFunction(initial, final, scene ,el);
                     }
                 }
             }
         }
     })
-    }
+}
